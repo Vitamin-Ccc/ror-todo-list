6a2ef98b55999962a22ca43edd29bc8a4e5b80fa
Sky Lin <linsky519@gmail.com>
2022-11-21 23:12:03 -0700

    list backend done

---
+++ app/models/list.rb
@@ -0,0 +1,4 @@
+class List < ApplicationRecord
+  validates :title, :desc, presence: true
+  validates :title, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Delete any list that was already there, optional,
+List.delete_all
+
+@counter = 0
+
+10.times do
+  List.create(
+    title: "#{Faker::Movie.title}#{@counter}", 
+    desc: Faker::Movie.quote
+  )
+  @counter += 1
+end
+
+# bundle exec rails db:seed
